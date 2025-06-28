e0138ed848204d3c60f4f5c21b27abe2762d6ce9
lumensss <137202110+lumensss@users.noreply.github.com>
2025-06-28 20:02:21 +0800

    add

---
+++ verif/sv/uvmtest.sv
@@ -0,0 +1,17 @@
+//  Class: cdn_cdrlftest 
+//
+class cdn_cdrlftest extends uvm_agent;
+    //  Group: Variables
+
+
+    //  Group: Constraints
+
+
+    //  Group: Functions
+
+    //  Constructor: new
+    function new(string name = "cdn_cdrlftest ");
+    endfunction: new
+    
+
+endclass: cdn_cdrlftest 
